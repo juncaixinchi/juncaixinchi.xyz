3115831af30e3dee34eb738d0d591d107054b7af
juncaixinchi <juncaixinchi111@gmail.com>
2016-12-24 18:44:01 +0800

    Add files via upload

---
+++ js/PlayMusic.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+function PlayMusic1()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=28285910&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
+function PlayMusic2()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=41665696&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
+function PlayMusic3()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=28285912&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
+function PlayMusic4()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=38392240&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
+function PlayMusic5()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=32303180&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
+function PlayMusic6()
+{
+document.getElementById("M1").src="http://music.163.com/outchain/player?type=2&id=411988502&auto=1&height=66";
+document.getElementById("M1").hidden=0;
+}
